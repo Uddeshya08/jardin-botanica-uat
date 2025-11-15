bf8f0594accdefa7f88ecdb96f3e0f985a74a909
vishal-kacha <vishaldineshbhaikacha@gmail.com>
2025-11-15 19:15:15 +0530

    added query params to handle checkout for razor pay

--- src/modules/checkout/templates/checkout-form/index.tsx
+++ src/modules/checkout/templates/checkout-form/index.tsx
@@ -1,18 +1,17 @@
 import { listCartShippingMethods } from "@lib/data/fulfillment"
 import { listCartPaymentMethods } from "@lib/data/payment"
 import { HttpTypes } from "@medusajs/types"
-import Addresses from "@modules/checkout/components/addresses"
-import Payment from "@modules/checkout/components/payment"
-import Review from "@modules/checkout/components/review"
-import Shipping from "@modules/checkout/components/shipping"
+import CheckoutFormClient from "./checkout-form-client"
+
+interface CheckoutFormProps {
+  cart: HttpTypes.StoreCart | null
+  customer: HttpTypes.StoreCustomer | null
+}
 
 export default async function CheckoutForm({
   cart,
   customer,
-}: {
-  cart: HttpTypes.StoreCart | null
-  customer: HttpTypes.StoreCustomer | null
-}) {
+}: CheckoutFormProps) {
   if (!cart) {
     return null
   }
@@ -25,14 +24,11 @@ export default async function CheckoutForm({
   }
 
   return (
-    <div className="w-full grid grid-cols-1 gap-y-8">
-      <Addresses cart={cart} customer={customer} />
-
-      <Shipping cart={cart} availableShippingMethods={shippingMethods} />
-
-      <Payment cart={cart} availablePaymentMethods={paymentMethods} />
-
-      <Review cart={cart} />
-    </div>
+    <CheckoutFormClient
+      cart={cart}
+      customer={customer}
+      shippingMethods={shippingMethods}
+      paymentMethods={paymentMethods}
+    />
   )
 }
